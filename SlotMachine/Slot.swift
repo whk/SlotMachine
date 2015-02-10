60db811096c6c4bf2837e7c7ebb046440a0f17be
Jim Kaubisch <jimkaubisch@Jims-MacBook-Pro15.local>
2015-02-10 11:53:21 -0800

    Initial Version

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,18 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Jim Kaubisch on 2/9/15.
+//  Copyright (c) 2015 Jim Kaubisch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
+
